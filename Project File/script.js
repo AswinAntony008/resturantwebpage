41abcc3c50eef589deba06dab6b99a5552d4c072
Aswin <132058319+AswinAntony008@users.noreply.github.com>
2024-12-28 15:30:24 +0530

    Add files via upload

---
+++ Project File/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper('.testimonials-slider', {
+   
+    autoplay: {
+      speed:200,
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+   
+  });
+});
